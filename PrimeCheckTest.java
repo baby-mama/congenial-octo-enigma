851f0269d41dbfb559491252f08d71165839cc24
Bogdan Klinowitzki <bklinovizkii2@gmail.com>
2025-01-19 18:24:00 +0100

    Test Update

--- PrimeCheckTest.java
+++ PrimeCheckTest.java
@@ -22,14 +22,20 @@ public class PrimeCheckTest {
   // --------------------------------------------------------------- //
   @Test
   public void testPrimes() {
-    /* ToDo: insert a test here */
-    assertEquals("this will fail", true, false);
+    assertEquals("Test 17", true, PrimeCheck.isPrime(17));
+    assertEquals("Test 19", true, PrimeCheck.isPrime(19));
+    assertEquals("Test 23", true, PrimeCheck.isPrime(23));
+    assertEquals("Test 37", true, PrimeCheck.isPrime(37));
+    assertEquals("Test 83", true, PrimeCheck.isPrime(83));
   }
 
   // --------------------------------------------------------------- //
   @Test
   public void testNonPrimes() {
-    /* ToDo: insert a test here */
-    assertEquals("this will fail", true, false);
+    assertEquals("Test 6", false, PrimeCheck.isPrime(6));
+    assertEquals("Test 8", false, PrimeCheck.isPrime(8));
+    assertEquals("Test 15", false, PrimeCheck.isPrime(15));
+    assertEquals("Test 56", false, PrimeCheck.isPrime(56));
+    assertEquals("Test 88", false, PrimeCheck.isPrime(88));
   }
 }
